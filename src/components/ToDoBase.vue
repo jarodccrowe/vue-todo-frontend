e9c7b134d493401fc8010bd919c9dea50fc3a450
jarod <jarodccrowe@gmail.com>
2019-12-07 17:56:03 +1100

    Update spacing

--- src/components/ToDoBase.vue
+++ src/components/ToDoBase.vue
@@ -3,31 +3,28 @@
     <h5>
       ToDo List
     </h5>
-    <div>
+    <div class="mb-2">
       {{ new Date() | moment("dddd, MMMM Do YYYY") }}
     </div>
-    <hr />
     <div>
       <div class="text-center pt-3" v-if="loading">
         <b-spinner small label="Small Spinner" type="grow" class="text-info"></b-spinner>
       </div>
-      <div>
+      <div class="incomplete-task-list mb-2">
         <div v-show="incompleteTasks.length === 0" class="text-muted">
           <small>
             <i>All tasks complete</i>
           </small>
         </div>
-        <div v-for="task in incompleteTasks" v-bind:key="task.id" class="mb-2">
-          <TaskBase v-bind="task" />
+        <div v-for="task in incompleteTasks" v-bind:key="task.id" class="task-wrapper">
+          <TaskBase v-bind="task"/>
         </div>
       </div>
-      <hr />
-      <div class="incomplete-task-list">
-        <div v-for="task in completeTasks" v-bind:key="task.id" class="mb-2">
-          <TaskBase v-bind="task" />
+      <div class="complete-task-list mb-4">
+        <div v-for="task in completeTasks" v-bind:key="task.id" class="">
+          <TaskBase v-bind="task" class="task"/>
         </div>
       </div>
-      <hr />
       <div>
         <AddTaskBase />
       </div>
@@ -78,7 +75,19 @@ li {
 a {
   color: #04b2e0;
 }
+.task-wrapper {
+  margin-bottom: 10px;
+}
+.task-wrapper:last-child {
+  margin-bottom: 0
+}
 .incomplete-task-list {
+  background-color:#eee;
+  padding: 7px 10px;
+  border-radius: 5px;
+}
+.complete-task-list {
   opacity: 0.5;
+  padding: 7px 10px;
 }
 </style>
