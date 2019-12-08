6134b6625c980d7af14ac94304659ad089fbe0a5
jarod <jarodccrowe@gmail.com>
2019-12-08 15:31:27 +1100

    Add vue-jest

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,10 @@
+// import { mount } from '@vue/test-utils'
+// import ToDoBase from '@/components/ToDoBase.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    // const wrapper = mount(ToDoBase)
+    // expect(wrapper.text()).toMatch(msg)
+    expect(1).toEqual(1);
+  })
+})
